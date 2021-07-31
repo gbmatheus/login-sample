d0057eb226d6191007528e554f6fa132f924a112
gbmatheus <gabrielmatheus2503@gmail.com>
2021-07-31 15:24:56 -0300

    chore: habilitando cors

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(routes);
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   res.json({ hello: 'world' });
 });
 
